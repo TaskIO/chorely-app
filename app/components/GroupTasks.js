dedc08efc16a551c7a67d20a06565ab6e8855bf9
Jason <jason.miguel@gmail.com>
2017-05-04 15:16:06 -0400

    added Single task and renamed new task

--- app/components/GroupTasks.js
+++ app/components/GroupTasks.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { StyleSheet, View, Text, Button } from 'react-native'
 
-export default class Group extends React.Component {
+export default class GroupTasks extends React.Component {
   render() {
     const { navigate } = this.props.navigation
     return (
       <View style={styles.container}>
         <Text>Group page!!</Text>
+        <ListView>Tasks</ListView>
         <Button title="Back to Home" onPress={() => navigate('Home')} />
       </View>
     );
