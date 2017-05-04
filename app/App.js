9e6207d416089b12e610a73c8b168ae41f203e34
Jason <jason.miguel@gmail.com>
2017-05-04 12:00:23 -0400

    basic input box

--- app/App.js
+++ app/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { StackNavigator } from 'react-navigation'
 import Home from './components/Home'
 import Group from './components/Group'
+import NewTask from './components/NewTask'
 
 const App = StackNavigator({
+  NewTask: {screen: NewTask},
   Home: {screen: Home},
-  // NewChore: {screen: NewChore},
   Group: {screen: Group}
 })
 
---
+++ app/components/NewTask.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { StyleSheet, View, Text, Button, TextInput } from 'react-native'
+
+export default class NewTask extends React.Component {
+  render() {
+    const { navigate } = this.props.navigation
+    return (
+      <TextInput
+        style={{height: 40, borderColor: "gray", borderWidth: 1}}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'salmon',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
