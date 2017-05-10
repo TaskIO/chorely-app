923d5f6b9ccd620e9b1de9e09460e377656e1b21
Daniel Fattal <dffattal@gmail.com>
2017-05-10 14:40:40 -0400

    fix: modify new task styling

--- app/views/taskTab/components/TaskForm.js
+++ app/views/taskTab/components/TaskForm.js
@@ -1,19 +1,32 @@
 'use strict'
 import React from 'react'
-import { Text, Input, Button, Container, Content, Header, Body, Title, Form, Item, Label } from 'native-base'
+import { Text, Input, Button, Container, Content, Header, Body, Title, Form, Item, Label, Picker } from 'native-base'
 import store from '../../../redux/store'
 import { addSelectedTask } from '../../../redux/reducers/tasks'
+
+const PickerItem = Picker.Item
 
 export default class TaskForm extends React.Component {
   constructor() {
     super()
     this.state = {
-      taskInput: ''
+      taskInput: '',
+      group: {}
     }
+    this.changeTaskInput = this.changeTaskInput.bind(this)
+    this.changeGroup = this.changeGroup.bind(this)
   }
-
+  changeTaskInput(taskInput) {
+    this.setState({
+      taskInput
+    })
+  }
+  changeGroup(group) {
+    this.setState({
+      group
+    })
+  }
   render() {
-    console.log(this.state)
     const { navigate, dispatch } = this.props.navigation;
     const taskInput = this.state.taskInput
     return (
@@ -24,23 +37,26 @@ export default class TaskForm extends React.Component {
               <Title>New Task</Title>
             </Body>
           </Header>
+          <Button />
           <Form>
             <Item stackedLabel>
               <Label>Task Description:</Label>
               <Input
-                onChangeText={newTaskInput => this.setState({taskInput: newTaskInput})}
+                onChangeText={this.changeTaskInput}
                 value={taskInput}
               />
             </Item>
           </Form>
-          <Button
-            onPress={ () => {
-              store.dispatch(addSelectedTask({name: taskInput}))
-              navigate('BountyForm')
-            }}
-            style={{flexDirection: 'row', justifyContent: 'center'}}>
-            <Text>{'Add Task'}</Text>
-          </Button>
+          <Body style={{flexDirection: 'row'}}>
+            <Button
+              onPress={ () => {
+                store.dispatch(addSelectedTask({name: taskInput}))
+                navigate('BountyForm')
+              }}
+              style={{flex: 1, maxWidth: 200}}>
+              <Text style={{textAlign: 'center'}}>{'Add Task'}</Text>
+            </Button>
+          </Body>
         </Content>
       </Container>
     );
