13fd3810efe9fc841131a5a66a5bba7608ac8077
Student <jeff.kandel@gmail.com>
2017-05-10 12:25:43 -0400

    feat: add logout butttom to profile for testing

--- app/views/userTab/components/Profile.js
+++ app/views/userTab/components/Profile.js
@@ -1,13 +1,27 @@
 'use strict'
 import React from 'react'
 import { Container, Content, Text, Button, Form, Item, Icon, Input, List, ListItem, Body, Label, Title, Header } from 'native-base';
+import { TouchableOpacity } from 'react-native'
 
 export default class Profile extends React.Component {
   render() {
+    const { navigate, dispatch } = this.props.navigation
     return (
       <Container>
         <Content>
           <Text>Profile page!</Text>
+          <TouchableOpacity
+          onPress={
+            () => navigate('Welcome')
+          }
+          style={{
+            padding:20,
+            borderRadius:20,
+            backgroundColor:'yellow',
+            marginTop:20
+          }}>
+          <Text>{'Logout'}</Text>
+          </TouchableOpacity>
         </Content>
       </Container>
     )
