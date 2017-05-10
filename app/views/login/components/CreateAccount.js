32abc2f61bf3d29e57cdc36e4b35c76c45787045
Student <jeff.kandel@gmail.com>
2017-05-10 14:30:54 -0400

    feat: add form to create Account

--- app/views/login/components/CreateAccount.js
+++ app/views/login/components/CreateAccount.js
@@ -1,25 +1,50 @@
 'use strict'
 import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { TouchableOpacity, View, StyleSheet, Dimensions, Platform  } from 'react-native'
+import { Button, Container, Text, Content, Form, Label, Item, Input } from 'native-base'
+import styles from './styles'
 
 export default class CreateAccount extends React.Component {
+  constructor(props){
+    super(props)
+
+  }
   render() {
     const { navigate, dispatch } = this.props.navigation
-    return(
-      <View>
-        <TouchableOpacity
-          onPress={
-            () => navigate('TabBarNavigation')
-          }
-          style={{
-            padding:20,
-            borderRadius:20,
-            backgroundColor:'yellow',
-            marginTop:20
-          }}>
-          <Text>{'Create Account'}</Text>
-          </TouchableOpacity>
-      </View>
+    return (
+      <Container>
+        <Content >
+          <View style={styles.formContainer}>
+          <Form>
+            <Item stackedLabel>
+              <Label>Username</Label>
+              <Input />
+            </Item>
+            <Item stackedLabel>
+              <Label>Email</Label>
+              <Input />
+            </Item>
+            <Item stackedLabel last>
+              <Label>Password</Label>
+              <Input />
+            </Item>
+          <Button
+            onPress={
+              () => navigate('TabBarNavigation')
+            }
+            >
+            <Text>{'Create Account'}</Text>
+            </Button>
+          </Form>
+          </View>
+        </Content>
+      </Container>
     )
   }
 }
+
+
+
+
+
+
