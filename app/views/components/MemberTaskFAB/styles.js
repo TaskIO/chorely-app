40b9cf20468f1c41ea6b1bca4af29651e315a82d
Student <jeff.kandel@gmail.com>
2017-05-15 11:04:18 -0400

    fix: better fab location

--- app/views/components/MemberTaskFAB/styles.js
+++ app/views/components/MemberTaskFAB/styles.js
@@ -3,7 +3,6 @@ import { cloudWhite, belizeBlue } from '../../../../constants/colors'
 export default {
   fab: {
     backgroundColor: cloudWhite,
-    left:5,
   },
   icon: {
     color: belizeBlue
--- app/views/components/SubmitFAB/styles.js
+++ app/views/components/SubmitFAB/styles.js
@@ -3,8 +3,6 @@ import { cloudWhite, belizeBlue } from '../../../../constants/colors'
 export default {
   fab: {
     backgroundColor: cloudWhite,
-    top:25,
-    left:5
   },
   icon:{
     color: belizeBlue
