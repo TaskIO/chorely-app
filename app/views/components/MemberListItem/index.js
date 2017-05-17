4cb29024d6a805713e9689aea8b3a37944713618
Jason <jason.miguel@gmail.com>
2017-05-17 11:12:57 -0400

    working with master

--- app/views/components/MemberListItem/index.js
+++ app/views/components/MemberListItem/index.js
@@ -1,15 +1,19 @@
 'use strict'
 import React from 'react'
-import { Text, ListItem, Body } from 'native-base'
+import { Text, ListItem, Body, Thumbnail } from 'native-base'
 import s from './styles'
 
-export default ({ member, navigate }) => {
+export default ({ member, navigate, userGroups }) => {
+  const userGroupInfo = userGroups.filter(userGroup => {
+    return userGroup.user.id === member.id
+  })
   return (
     <ListItem>
+      <Thumbnail source={member.image === 'default.png' ? require('../../../theme/img/default.png') : {uri: member.image}} />
       <Body >
         <Text style={s.mainText}>{member.name}</Text>
-      <Text note style={s.parenthetical} > Points: {member.points} </Text>
-    </Body>
+        <Text note style={s.parenthetical} > Points: {userGroupInfo[0].points} </Text>
+      </Body>
     </ListItem>
   )
 }
